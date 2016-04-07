f04a25b5074e7c8fe38c083d7669b7ad0e3904b5
matthew.page <matthew.page@digital.hmrc.gov.uk>
2016-04-07 08:35:19 +0100

    CGT-647: Refactored to remove namespace

---
+++ test/controllers/MessageControllerSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.http.Status
+import play.api.test.FakeRequest
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+
+class MessageControllerSpec extends UnitSpec with WithFakeApplication {
+
+  val fakeRequest = FakeRequest("GET", "/capital-gains-calculator/hello-world")
+
+  "GET /capital-gains-calculator/hello-world" should {
+    "return 200" in {
+      val result = MessageController.helloWorld()(fakeRequest)
+      status(result) shouldBe Status.OK
+    }
+  }
+}
