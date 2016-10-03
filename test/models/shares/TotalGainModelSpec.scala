01cc5c58a9aac5a91bd60db0c7298b196ddc2575
James Forster <james.forster@digital.hmrc.gov.uk>
2016-10-03 11:03:57 +0100

    Added additional binder tests.

--- test/models/shares/TotalGainModelSpec.scala
+++ test/models/shares/TotalGainModelSpec.scala
@@ -18,15 +18,17 @@ package models.shares
 
 import models.resident.shares.TotalGainModel
 import org.mockito.Matchers
+import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import play.api.mvc.QueryStringBindable
 import uk.gov.hmrc.play.test.UnitSpec
-import org.mockito.Mockito._
 
 
 class TotalGainModelSpec extends UnitSpec with MockitoSugar {
 
-  def setupMockDoubleBinder(bindValue: Option[Either[String, Double]], unbindValue: String): QueryStringBindable[Double] = {
+  def setupMockDoubleBinder(bindValue: Option[Either[String, Double]],
+                            unbindValue: String,
+                            bindValues: (String, Option[Either[String, Double]])*): QueryStringBindable[Double] = {
 
     val mockBinder = mock[QueryStringBindable[Double]]
 
@@ -36,13 +38,23 @@ class TotalGainModelSpec extends UnitSpec with MockitoSugar {
     when(mockBinder.unbind(Matchers.any(), Matchers.any()))
       .thenReturn(unbindValue)
 
+    bindValues.foreach(input =>
+      when(mockBinder.bind(Matchers.eq(input._1), Matchers.any()))
+        .thenReturn(input._2)
+    )
+
+    bindValues.filter(_._2.get.isRight).foreach(input =>
+      when(mockBinder.unbind(Matchers.eq(input._1), Matchers.any()))
+        .thenReturn(input._2.get.right.get.toString)
+    )
+
     mockBinder
   }
 
   "Calling TotalGain binder" when {
 
-    "given a valid binding value" should {
-      implicit val mockDoubleBinder = setupMockDoubleBinder(Some(Right(1000.0)), "1000")
+    "given a valid binding value where all values are the same" should {
+      implicit val mockDoubleBinder = setupMockDoubleBinder(Some(Right(1000.0)), "1000.0")
       val binder = TotalGainModel.totalGainBinder
 
       "return a valid TotalGainModel on bind" in {
@@ -54,18 +66,44 @@ class TotalGainModelSpec extends UnitSpec with MockitoSugar {
       "return a valid queryString on unbind" in {
         val result = binder.unbind("key", TotalGainModel(1000.0, 1000.0, 1000.0, 1000.0))
 
-        result shouldBe "disposalValue=1000&disposalCosts=1000&acquisitionValue=1000&acquisitionCosts=1000"
+        result shouldBe "disposalValue=1000.0&disposalCosts=1000.0&acquisitionValue=1000.0&acquisitionCosts=1000.0"
+      }
+    }
+
+    "given a valid binding value where all values are different" should {
+      implicit val mockDoubleBinder = setupMockDoubleBinder(Some(Right(2000.0)), "2000.0", ("disposalCosts", Some(Right(2500.0))),
+        ("acquisitionValue", Some(Right(3000.0))), ("acquisitionCosts", Some(Right(3500.0))))
+      val binder = TotalGainModel.totalGainBinder
+
+      "return a valid TotalGainModel on bind" in {
+        val result = binder.bind("Any", Map("key" -> Seq("data")))
+
+        result shouldBe Some(Right(TotalGainModel(2000.0, 2500.0, 3000.0, 3500.0)))
+      }
+
+      "return a valid queryString on unbind" in {
+        val result = binder.unbind("key", TotalGainModel(2000.0, 2500.0, 3000.0, 3500.0))
+
+        result shouldBe "disposalValue=2000.0&disposalCosts=2500.0&acquisitionValue=3000.0&acquisitionCosts=3500.0"
       }
     }
 
     "given an invalid binding value" should {
-      implicit val mockDoubleBinder = setupMockDoubleBinder(Some(Left("Error message")), "")
-      val binder = TotalGainModel.totalGainBinder
 
-      "return an error message on a failed bind" in {
+      "return one error message on a failed bind on all inputs" in {
+        implicit val mockDoubleBinder = setupMockDoubleBinder(Some(Left("Error message")), "")
+        val binder = TotalGainModel.totalGainBinder
         val result = binder.bind("Any", Map("key" -> Seq("data")))
 
         result shouldBe Some(Left("Error message"))
+      }
+
+      "return an error message when one component fails" in {
+        implicit val mockDoubleBinder = setupMockDoubleBinder(Some(Right(1000.0)), "", ("disposalValue", Some(Left("Invalid disposal value"))))
+        val binder = TotalGainModel.totalGainBinder
+        val result = binder.bind("Any", Map("key" -> Seq("data")))
+
+        result shouldBe Some(Left("Invalid disposal value"))
       }
     }
   }
