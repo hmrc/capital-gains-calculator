ed361b29047fff36eead97e10cda594fec1d7822
stephen.ward <stephen.ward@digital.hmrc.gov.uk>
2017-10-06 14:57:27 +0100

    Great Library Upgrade

--- project/microServiceBuild.scala
+++ project/microServiceBuild.scala
@@ -35,18 +35,14 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val hmrcTestVersion = "2.3.0"
+  private val hmrcTestVersion = "2.4.0"
   private val jsoupVersion = "1.10.2"
   private val pegDownVersion = "1.6.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.15.0",
-    "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
-    "uk.gov.hmrc" %% "play-health" % "2.1.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "6.9.0",
     "uk.gov.hmrc" %% "play-url-binders" % "2.1.0",
-    "uk.gov.hmrc" %% "play-config" % "4.3.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "uk.gov.hmrc" %% "domain" % "4.1.0",
     "joda-time" % "joda-time" % "2.9.6"
   )
