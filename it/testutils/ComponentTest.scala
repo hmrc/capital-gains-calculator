7d6c8d23cdfd0f28be9147cae2b7c5f5f1c0541b
Kieran Puaar <Kieran.Puaar@digital.hmrc.gov.uk>
2018-03-13 14:03:43 +0000

    Adding Component Tests for Non Resident

--- it/testutils/ComponentTest.scala
+++
@@ -1,5 +0,0 @@
-package testutils
-
-trait ComponentTest {
-
-}
---
+++ it/testutils/nonResident/NonResidentComponentTest.scala
@@ -0,0 +1,98 @@
+package testutils.nonResident
+
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
+import play.api.test.Helpers._
+import play.api.libs.ws.{WSClient, WSResponse}
+import uk.gov.hmrc.play.test.UnitSpec
+
+class NonResidentComponentTest extends UnitSpec with GuiceOneServerPerSuite {
+
+  override implicit lazy val app: Application = new GuiceApplicationBuilder()
+    .build()
+
+  val calculateUrl = s"http://localhost:$port/calculate-your-capital-gains/non-resident/"
+
+  lazy val ws: WSClient = app.injector.instanceOf[WSClient]
+
+  "Hitting the /non-resident/calculate-total-gain route" should {
+    s"return a $OK with a valid result" when {
+      "non-residential" in {
+        def request: WSResponse = ws.url(calculateUrl)
+          .post(
+            Json.parse(
+              """
+                |{
+                |
+                |}
+              """.
+                stripMargin)
+          )
+
+        val responseJson = Json.parse(
+          """
+            |{
+            |
+            |}
+          """.stripMargin)
+
+        request.status shouldBe OK
+        request.json shouldBe responseJson
+      }
+    }
+
+    s"return a $BAD_REQUEST" when {
+      "Data is missing" in {
+        //bad request / data is missing / 400
+        def request: WSResponse = ws.url(calculateUrl)
+          .post(
+            Json.parse(
+              """
+                |{
+                |
+                |}
+              """.
+                stripMargin)
+          )
+
+        val responseJson = Json.parse(
+          """
+            |{
+            |
+            |}
+          """.stripMargin)
+
+        request.status shouldBe 400
+        request.json shouldBe responseJson
+      }
+    }
+
+    "return a 500 status" when {
+      "an unexpected error occurs" in {
+        //unexpected / server side / 500
+        def request: WSResponse = ws.url(calculateUrl)
+          .post(
+            Json.parse(
+              """
+                |{
+                |
+                |}
+              """.
+                stripMargin)
+          )
+
+        val responseJson = Json.parse(
+          """
+            |{
+            |
+            |}
+          """.stripMargin)
+
+        request.status shouldBe 500
+        request.json shouldBe responseJson
+      }
+    }
+  }
+}
